7897cde275ef7e128da8332cecb0d2f2b86d744c
nhanptt-0488 <phan.thi.thanh.nhan@sun-asterisk.com>
2024-07-25 17:29:13 +0700

    feat: layout admin page

--- app/helpers/components/dialog_helper.rb
+++ app/helpers/components/dialog_helper.rb
@@ -1,14 +1,14 @@
 module Components::DialogHelper
   def render_dialog(**options, &block)
     content = capture(&block) if block
-    render "components/ui/dialog", content: content, **options
+    render "components/ui/dialog", content:, **options
   end
 
-  def dialog_trigger(&block)
+  def dialog_trigger &block
     content_for :dialog_trigger, capture(&block), flush: true
   end
 
-  def dialog_content(&block)
+  def dialog_content &block
     content_for :dialog_content, capture(&block), flush: true
   end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -2,5 +2,6 @@ class Author < ApplicationRecord
   has_many :books
   has_many :author_followers
 
-  validates :name, :birth, :gender, :bio, :nationality, :profile_url, presence: true
+  validates :name, :birth, :gender, :bio, :nationality, :profile_url,
+            presence: true
 end
