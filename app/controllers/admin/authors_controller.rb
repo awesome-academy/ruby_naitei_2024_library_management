8eed9fb0d268788cd469ef2e90d0ec6204dc8c4c
Doan1412 <85919961+Doan1412@users.noreply.github.com>
2024-08-03 17:11:51 +0700

    Admin_add_author

--- app/controllers/admin/authors_controller.rb
+++ app/controllers/admin/authors_controller.rb
@@ -3,4 +3,25 @@ class Admin::AuthorsController < Admin::ApplicationController
     @pagy, @authors = pagy Author.order_by_name,
                            items: Settings.authors_per_page
   end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new author_params
+    if @author.save
+      flash[:success] = t("noti.author.created_success")
+      redirect_to admin_authors_path
+    else
+      flash[:warning] = t("noti.author.created_fail")
+      redirect_to new_admin_author_path
+    end
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit Author::AUTHOR_PARAMS
+  end
 end
