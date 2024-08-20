879af559f72f9f3c08859d5dc66bea9aa01b1c88
Doan1412 <85919961+Doan1412@users.noreply.github.com>
2024-08-20 23:42:38 +0700

    update_avatar

---
+++ app/javascript/custom/image_preview.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const fileInput = document.querySelector('.field');
+  const previewImage = document.getElementById('image-preview');
+  const dropzone = document.querySelector('#dropzone');
+
+  fileInput.addEventListener('change', function(event) {
+    const file = event.target.files[0];
+
+    if (file && (file.type === 'image/png' || file.type === 'image/jpeg')) {
+      const reader = new FileReader();
+
+      reader.onload = function(e) {
+        previewImage.src = e.target.result;
+        previewImage.classList.remove('hidden');
+        dropzone.classList.add('hidden');
+      };
+
+      reader.readAsDataURL(file);
+    } else {
+      previewImage.src = '#';
+      previewImage.classList.add('hidden');
+    }
+  });
+});
